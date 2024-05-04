be732b573606838b617917095756434eb459edd4
Hemanya Sharma <hemanya.sharma@zoho.com>
2024-05-04 13:29:04 -0400

    Adjust le feel

--- Car.h
+++ Car.h
@@ -49,9 +49,9 @@ class Car {
     bool running;  // Is car running
 
     // Gearing
-    float gearRatios[7] = {0, 0.8, 1.4, 1.6, 1.9, 2.4, 2.8};
+    float gearRatios[7] = {0, 1.1, 1.4, 1.6, 1.9, 2.4, 2.8};
     float gearLazyValues[7] = {0.99, 0.999, 0.9995, 0.9996, 0.9997, 0.9998, 0.9999};
-    float gearThrottleResponses[7] = {1, 0.1, 0.06, 0.05, 0.04, 0.035, 0.03};
+    float gearThrottleResponses[7] = {1, 0.15, 0.08, 0.06, 0.05, 0.04, 0.035};
 
     // Engine
     double lazyValue = 0.99;      // Engine time for revs to settle back - values closer to one need more time to go back to idle
