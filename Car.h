41e37f66a29ddd73a12620ca16a6fc9ec1fa3c7c
Hemanya Sharma <hemanya.sharma@zoho.com>
2024-05-04 00:02:06 -0400

    Update formatting

--- Car.h
+++ Car.h
@@ -11,149 +11,134 @@
 
 // Averages float values
 class Damper {
-private:
-  std::queue<double> values;
-  int maxSize;
+   private:
+    std::queue<double> values;
+    int maxSize;
 
-public:
-  Damper(int size) : maxSize(size) {}
+   public:
+    Damper(int size) : maxSize(size) {}
 
-  void addValue(double value) {
-    values.push(value);
-    if (values.size() > maxSize) {
-      values.pop();
-    }
-  }
-
-  double getAverage() {
-    if (values.empty()) {
-      return 0.0; // Return 0 if queue is empty
+    void addValue(double value) {
+        values.push(value);
+        if (values.size() > maxSize) {
+            values.pop();
+        }
     }
 
-    double sum = 0.0;
-    int count = 0;
-    std::queue<double> temp = values; // Create a copy of the queue
+    double getAverage() {
+        if (values.empty()) {
+            return 0.0;  // Return 0 if queue is empty
+        }
 
-    while (!temp.empty()) {
-      sum += temp.front();
-      temp.pop();
-      count++;
+        double sum = 0.0;
+        int count = 0;
+        std::queue<double> temp = values;  // Create a copy of the queue
+
+        while (!temp.empty()) {
+            sum += temp.front();
+            temp.pop();
+            count++;
+        }
+
+        return sum / count;
     }
-
-    return sum / count;
-  }
 };
 
 class Car {
-public:
-  bool running; // Is car running
+   public:
+    bool running;  // Is car running
 
-  // Gearing
-  float gearRatios[7] = {0, 0.8, 1.4, 1.6, 1.9, 2.4, 2.8};
-  float gearLazyValues[7] = {0.99,   0.999,  0.9995, 0.9996,
-                             0.9997, 0.9998, 0.9999};
-  float gearThrottleResponses[7] = {1, 0.1, 0.06, 0.05, 0.04, 0.035, 0.03};
+    // Gearing
+    float gearRatios[7] = {0, 0.8, 1.4, 1.6, 1.9, 2.4, 2.8};
+    float gearLazyValues[7] = {0.99, 0.999, 0.9995, 0.9996, 0.9997, 0.9998, 0.9999};
+    float gearThrottleResponses[7] = {1, 0.1, 0.06, 0.05, 0.04, 0.035, 0.03};
 
-  // Engine
-  double lazyValue = 0.99; // Engine time for revs to settle back - values
-                           // closer to one need more time to go back to idle
-  double throttleResponse =
-      1; // Throttle sensitivity - Should feel lower in higher gears since we
-         // reduce the value after every upshift bruh
+    // Engine
+    double lazyValue = 0.99;      // Engine time for revs to settle back - values closer to one need more time to go back to idle
+    double throttleResponse = 1;  // Throttle sensitivity - Should feel lower in higher gears since we reduce the value after every upshift bruh
 
-  // Wheel
-  double coastLazyValue =
-      0.999;             // Driving drag on wheels (and also engine if in gear)
-  float brakeFactor = 1; // Brake Drag on wheels (and also engine if in gear) -
-                         // Basically the same as coastLazyValue
+    // Wheel
+    double coastLazyValue = 0.999;  // Driving drag on wheels (and also engine if in gear)
+    float brakeFactor = 1;          // Brake Drag on wheels (and also engine if in gear) - Basically the same as coastLazyValue
 
-  std::mutex m_tick;
-  void tick() {
-    while (running) {
-      std::lock_guard<std::mutex> lock(m_tick);
+    std::mutex m_tick;
+    void tick() {
+        while (running) {
+            // TODO: Fix broken multithreading
+            std::lock_guard<std::mutex> lock(m_tick);
 
-      if (rpm >= 800) { // Idle air control valve
-        idleValve = 1;
-      } else if (rpm <= 700) {
-        idleValve = 30;
-      }
-      rpm = rpm * lazyValue;
-      if (rpm > 0) {
-        rpm += horses / rpm;
-        if (rpm <= 8000) { // Rev limiter thingy
-          horses = rpm * (gas + idleValve) * throttleResponse * brakeFactor;
-        } else {
-          horses = 0;
+            if (rpm >= 800) {  // Idle air control valve
+                idleValve = 1;
+            } else if (rpm <= 700) {
+                idleValve = 30;
+            }
+            rpm = rpm * lazyValue;
+            if (rpm > 0) {
+                rpm += horses / rpm;
+                if (rpm <= 8000) {  // Rev limiter thingy
+                    horses = rpm * (gas + idleValve) * throttleResponse * brakeFactor;
+                } else {
+                    horses = 0;
+                }
+            }
+
+            // Apply clutch revs in multiple smaller chunks of revs, makes a kick, like dumping the clutch in a real car
+            // Idk how this works
+            rpm += clutch * clutchKick;
+            clutch = (1.0 - clutchKick) * clutch;
+
+            // Wheel RPM depending on the engine rpm , current gear ratio and coasting drag
+            if (gear >= 1) {
+                wheelRPM = rpm * gearRatios[gear] * coastLazyValue * brakeFactor;
+                rpm = rpm * brakeFactor;
+            } else {
+                wheelRPM = wheelRPM * coastLazyValue * brakeFactor;
+            }
+
+            rpmDamper.addValue(rpm);
+            wheelSpeedDamper.addValue(wheelRPM);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));  // Farts per second
         }
-      }
-
-      // Apply clutch revs in multiple smaller chunks of revs, makes a kick,
-      // like dumping the clutch in a real car Idk how this works
-      rpm += clutch * clutchKick;
-      clutch = (1.0 - clutchKick) * clutch;
-
-      // Wheel RPM depending on the engine rpm , current gear ratio and coasting
-      // drag
-      if (gear >= 1) {
-        wheelRPM = rpm * gearRatios[gear] * coastLazyValue * brakeFactor;
-        rpm = rpm * brakeFactor;
-      } else {
-        wheelRPM = wheelRPM * coastLazyValue * brakeFactor;
-      }
-
-      rpmDamper.addValue(rpm);
-      wheelSpeedDamper.addValue(wheelRPM);
-
-      std::this_thread::sleep_for(
-          std::chrono::milliseconds(10)); // Farts per second
     }
-  }
-  void setGear(int newGear) {
-    gear = newGear;
-    if (gear > 0) {
-      clutch = wheelRPM / gearRatios[gear] - rpm;
-      if (wheelRPM <= 0) {
-        clutch = 500 - rpm; // rpm + clutch would be 500 (since clutch takes a
-                            // rev difference that it smoothly applies)
-        return;
-      }
+
+    void setGear(int newGear) {
+        gear = newGear;
+        if (gear > 0) {
+            clutch = wheelRPM / gearRatios[gear] - rpm;
+            if (wheelRPM <= 0) {
+                clutch = 500 - rpm;  // rpm + clutch would be 500 (since clutch takes a rev difference that it smoothly applies)
+                return;
+            }
+        }
     }
-  }
+    int getGear() { return gear; }
 
-  int getGear() { return gear; }
+    void setGas(float newGas) { gas = newGas; }
+    float getGas() { return gas; }
 
-  void setGas(float newGas) { gas = newGas; }
-  float getGas() { return gas; }
+    void setRPM(float newRPM) { rpm = newRPM; }  // Sets rpm for next tick
+    float getRPM() { return rpmDamper.getAverage(); }
 
-  void setRPM(float newRPM) {
-    // Sets rpm for next tick
-    rpm = newRPM;
-  }
-  float getRPM() { return rpmDamper.getAverage(); }
+    void setWheelSpeed(float newSpeed) { wheelRPM = newSpeed; }  // Sets wheelRPM for next tick
+    float getWheelSpeed() { return wheelSpeedDamper.getAverage(); }
 
-  float getWheelSpeed() { return wheelSpeedDamper.getAverage(); }
+    float getHorses() { return horses; }
 
-  void setWheelSpeed(float newSpeed) {
-    // Sets wheelRPM for next tick
-    wheelRPM = newSpeed;
-  }
+   private:
+    float gas = 0;       // Throttle body
+    float rpm = 0;       // Engine RPM
+    float wheelRPM = 0;  // Wheel RPM or speed does not really matter
 
-  float getHorses() { return horses; }
+    float idleValve = 1;  // Idle valve
+    float horses = 0;     // Power produced immediately
+    int gear = 0;         // Gearing
 
-private:
-  float gas = 0;      // Throttle body
-  float rpm = 0;      // Engine RPM
-  float wheelRPM = 0; // Wheel RPM or speed does not really matter
+    float clutch = 0;  // Difference of revs to 'smoothly' join
+    float clutchKick = 0.3;
 
-  float idleValve = 1; // Idle valve
-  float horses = 0;    // Power produced immediately
-  int gear = 0;        // Gearing
-
-  float clutch = 0; // Difference of revs to 'smoothly' join
-  float clutchKick = 0.3;
-
-  Damper rpmDamper{5};
-  Damper wheelSpeedDamper{10};
+    Damper rpmDamper{5};
+    Damper wheelSpeedDamper{10};
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "Car.h"
 #include <SFML/Config.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Time.hpp>
@@ -7,154 +6,158 @@
 #include <map>
 #include <string>
 #include <thread>
+
+#include "Car.h"
 // #include <fmod.hpp>
 
 int main() {
+    Car car;
+    car.running = true;
 
-  Car car;
-  car.running = true;
+    // Car
+    std::thread vroom(&Car::tick, std::ref(car));
 
-  // Car
-  std::thread vroom(&Car::tick, std::ref(car));
+    // Create a window
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "VROOM");
+    sf::Clock clock;  // For FPS display
 
-  // Create a window
-  sf::RenderWindow window(sf::VideoMode(1024, 768), "VROOM");
+    // Text Information
+    sf::Font font;
+    if (!font.loadFromFile("Race-Sport.ttf")) {
+        car.running = false;
+        vroom.join();
+        std::cerr << "Failed to load font" << std::endl;
+        return EXIT_FAILURE;
+    }
+    sf::Text gaugeValue;
+    gaugeValue.setFont(font);
+    gaugeValue.setCharacterSize(16);
+    gaugeValue.setFillColor(sf::Color::White);
+    gaugeValue.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
 
-  // Guage information
-  sf::Font font;
-  if (!font.loadFromFile("Race-Sport.ttf")) {
-    car.running = false;
-    vroom.join();
-    std::cerr << "Failed to load font" << std::endl;
-    return EXIT_FAILURE;
-  }
-  sf::Text gaugeValue;
-  gaugeValue.setFont(font);
-  gaugeValue.setCharacterSize(16);
-  gaugeValue.setFillColor(sf::Color::White);
-  gaugeValue.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    // Tachometer graphics
+    sf::Texture texture;
+    if (!texture.loadFromFile("race_tach.png")) {
+        car.running = false;
+        vroom.join();
+        return EXIT_FAILURE;
+    }
+    sf::Sprite sprite(texture);
+    sprite.setOrigin(sprite.getLocalBounds().width / 2.f, sprite.getLocalBounds().height / 2.f);
+    sprite.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    sprite.setScale(0.3f, 0.3f);
 
-  sf::Clock clock; // For FPS
+    // Tachometer needle
+    sf::RectangleShape tach(sf::Vector2f(250.f, 6.f));  // Size of the tach
+    tach.setFillColor(sf::Color::Red);                  // Color of the tach
+    tach.setPosition(1024.f / 2.f, 768.f / 2.f);        // Position of the tach
+    tach.setOrigin(250.f, 3.f);                         // Center of rotation
 
-  // Tachometer graphics
-  sf::Texture texture;
-  if (!texture.loadFromFile("race_tach.png")) {
-    car.running = false;
-    vroom.join();
-    return EXIT_FAILURE;
-  }
-  sf::Sprite sprite(texture);
-  sprite.setOrigin(sprite.getLocalBounds().width / 2.f,
-                   sprite.getLocalBounds().height / 2.f);
-  sprite.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
-  sprite.setScale(0.3f, 0.3f);
+    // Speedometer meter
+    sf::RectangleShape speedo(sf::Vector2f(150.f, 6.f));   // Size of the speedo
+    speedo.setFillColor(sf::Color::Red);                   // Color of the speedo
+    speedo.setPosition(1024.f / 2.f, 768.f / 2.f - 75.f);  // Position of the speedo
+    speedo.setOrigin(150.f, 3.f);                          // Center of rotation
 
-  // Tachometer needle
-  sf::RectangleShape tach(sf::Vector2f(250.f, 6.f)); // Size of the tach
-  tach.setFillColor(sf::Color::Red);                 // Color of the tach
-  tach.setPosition(1024.f / 2.f, 768.f / 2.f);       // Position of the tach
-  tach.setOrigin(250.f, 3.f);                        // Center of rotation
+    // Map user keyboard input into differen levels of throttle
+    std::map<sf::Keyboard::Key, int> userThrottleMap;
+    userThrottleMap[sf::Keyboard::Key::Q] = 30;
+    userThrottleMap[sf::Keyboard::Key::W] = 50;
+    userThrottleMap[sf::Keyboard::Key::E] = 80;
+    userThrottleMap[sf::Keyboard::Key::R] = 130;
+    userThrottleMap[sf::Keyboard::Key::T] = 150;
 
-  // Speedometer meter
-  sf::RectangleShape speedo(sf::Vector2f(150.f, 6.f)); // Size of the speedo
-  speedo.setFillColor(sf::Color::Red);                 // Color of the speedo
-  speedo.setPosition(1024.f / 2.f,
-                     768.f / 2.f - 75.f); // Position of the speedo
-  speedo.setOrigin(150.f, 3.f);           // Center of rotation
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event;
 
-  // Map user keyboard input into differen levels of throttle
-  std::map<sf::Keyboard::Key, int> userThrottleMap;
-  userThrottleMap[sf::Keyboard::Key::Q] = 30;
-  userThrottleMap[sf::Keyboard::Key::W] = 50;
-  userThrottleMap[sf::Keyboard::Key::E] = 80;
-  userThrottleMap[sf::Keyboard::Key::R] = 130;
-  userThrottleMap[sf::Keyboard::Key::T] = 150;
+        sf::Time elapsed = clock.restart();
+        float fps = 1.0f / elapsed.asSeconds();
 
-  while (window.isOpen()) {
-    sf::Event event;
-    sf::Time elapsed = clock.restart();
-    float fps = 1.0f / elapsed.asSeconds();
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
-      // Key press events
-      if (event.type == sf::Event::KeyPressed) {
-        auto it = userThrottleMap.find(event.key.code);
-        if (it != userThrottleMap.end()) {
-          std::cout << "Accelerator at " << it->second << " \n";
-          car.setGas(it->second);
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            // Key press events
+            if (event.type == sf::Event::KeyPressed) {
+                // Accelerator options
+                auto it = userThrottleMap.find(event.key.code);
+                if (it != userThrottleMap.end()) {
+                    std::cout << "Accelerator at " << it->second << " \n";
+                    car.setGas(it->second);
+                }
+                // Upshift
+                if (event.key.code == sf::Keyboard::Key::Up) {
+                    std::cout << "Shifted Up\n";
+                    car.setGear(car.getGear() + 1);
+                    car.lazyValue = car.gearLazyValues[car.getGear()];
+                    car.throttleResponse = car.gearThrottleResponses[car.getGear()];
+                }
+                // Downshift
+                if (event.key.code == sf::Keyboard::Key::Down) {
+                    std::cout << "Shifted Down\n";
+                    car.setGear(car.getGear() - 1);
+                    car.lazyValue = car.gearLazyValues[car.getGear()];
+                    car.throttleResponse = car.gearThrottleResponses[car.getGear()];
+                }
+                // Shift to N
+                if (event.key.code == sf::Keyboard::Key::LShift) {
+                    std::cout << "To neutral\n";
+                    car.setGear(0);
+                    car.lazyValue = car.gearLazyValues[car.getGear()];
+                    car.throttleResponse = car.gearThrottleResponses[car.getGear()];
+                }
+                // Brakes
+                if (event.key.code == sf::Keyboard::Key::Period) {
+                    std::cout << "Brakes on\n";
+                    car.brakeFactor = 0.996;
+                }
+                // Crude Starter
+                if (event.key.code == sf::Keyboard::Key::S) {
+                    // One strong starter
+                    car.setRPM(500);
+                    std::cout << "Starter\n";
+                }
+            }
+            // Key release events
+            if (event.type == sf::Event::KeyReleased) {
+                // If one of the keys in our throttle map is released, release the throttle
+                auto it = userThrottleMap.find(event.key.code);
+                if (it != userThrottleMap.end()) {
+                    std::cout << "Accelerator released\n";
+                    car.setGas(0);
+                }
+                // Disengage brakes
+                if (event.key.code == sf::Keyboard::Key::Period) {
+                    std::cout << "Brakes off\n";
+                    car.brakeFactor = 1;
+                }
+            }
         }
-        if (event.key.code == sf::Keyboard::Key::Up) {
-          std::cout << "Shifted Up\n";
-          car.setGear(car.getGear() + 1);
-          car.lazyValue = car.gearLazyValues[car.getGear()];
-          car.throttleResponse = car.gearThrottleResponses[car.getGear()];
-        }
-        if (event.key.code == sf::Keyboard::Key::Down) {
-          std::cout << "Shifted Down\n";
-          car.setGear(car.getGear() - 1);
-          car.lazyValue = car.gearLazyValues[car.getGear()];
-          car.throttleResponse = car.gearThrottleResponses[car.getGear()];
-        }
-        if (event.key.code == sf::Keyboard::Key::LShift) {
-          std::cout << "To neutral\n";
-          car.setGear(0);
-          car.lazyValue = car.gearLazyValues[car.getGear()];
-          car.throttleResponse = car.gearThrottleResponses[car.getGear()];
-        }
-        if (event.key.code == sf::Keyboard::Key::Period) {
-          std::cout << "Brakes on\n";
-          car.brakeFactor = 0.996;
-        }
 
-        if (event.key.code == sf::Keyboard::Key::S) {
-          // One strong starter
-          car.setRPM(500);
-          std::cout << "Starter\n";
-        }
-      }
-      // Key release events
-      if (event.type == sf::Event::KeyReleased) {
-        // If one of the keys in out throttle map is released, release the
-        // throttle
-        auto it = userThrottleMap.find(event.key.code);
-        if (it != userThrottleMap.end()) {
-          std::cout << "Accelerator released\n";
-          car.setGas(0);
-        }
-        if (event.key.code == sf::Keyboard::Key::Period) {
-          std::cout << "Brakes off\n";
-          car.brakeFactor = 1;
-        }
-      }
+        // Move needles
+        tach.setRotation(car.getRPM() / 30 - 90);
+        speedo.setRotation(car.getWheelSpeed() / 100);
+        // Set gauge display
+        gaugeValue.setString(std::to_string((int)car.getRPM()) + " RPM\n" + std::to_string((int)car.getHorses()) + " Horses\n" + std::to_string(car.getGear()) + "\n" + std::to_string((int)car.getWheelSpeed() / 100) + " kmh\n" + std::to_string((int)fps) +
+                             " FPS");
+
+        window.clear(sf::Color::Black);
+
+        window.draw(sprite);
+        window.draw(speedo);
+        window.draw(tach);
+        window.draw(gaugeValue);
+
+        window.display();
+
+        // TODO: Add a proper fps limiter
+        sf::Time sleepTime = sf::seconds(0.01);
+        sf::sleep(sleepTime);
     }
 
-    tach.setRotation(car.getRPM() / 30 - 90);
-    speedo.setRotation(car.getWheelSpeed() / 100);
+    car.running = false;
+    vroom.join();
 
-    gaugeValue.setString(std::to_string((int)car.getRPM()) + " RPM\n" +
-                         std::to_string((int)car.getHorses()) + " Horses\n" +
-                         std::to_string(car.getGear()) + "\n" +
-                         std::to_string((int)car.getWheelSpeed() / 100) +
-                         " kmh\n" + std::to_string((int)fps) + " FPS");
-
-    window.clear(sf::Color::Black);
-
-    window.draw(sprite);
-    window.draw(speedo);
-    window.draw(tach);
-    window.draw(gaugeValue);
-
-    window.display();
-
-    // TODO: Add a proper fps limiter
-    sf::Time sleepTime = sf::seconds(0.01);
-    sf::sleep(sleepTime);
-  }
-
-  car.running = false;
-  vroom.join();
-
-  return 0;
+    return 0;
 }
